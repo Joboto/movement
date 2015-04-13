7bbe7e271821164b89d9291a09e9c626acdd562d
Joboto <joeisamonkey@gmail.com>
2015-04-13 21:27:43 +0100

    Making use of localStorage
    Log in now stores returned JSON from member in local storage.
    Would want this to not include password in future.
    Figured out how to retrieve values from JSON;
    now to try and pass them to login.php, perhaps via session.

--- scripts/move.js
+++ scripts/move.js
@@ -35,10 +35,21 @@ function logmein(){
 				alert("Could not find, please try again");
 			} else {
 				alert("Welcome "+obj[0].fname+" "+obj[0].lname);
+				localStorage.setItem('loggedOn', JSON.stringify(obj[0]));
 			}
 		},
 		error: function() {
 			alert("Please try again.");
 		}
 	});
+}
+
+function logmeout(){
+	localStorage.clear();
+}
+
+function whologgedon(){
+	var thisone = localStorage.getItem('loggedOn');
+	alert("You are "+(JSON.parse(thisone)).fname);
+	console.log(JSON.parse(thisone));
 }
