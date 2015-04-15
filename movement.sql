2703bc2b026c3e0235f28e437f0acf176e7097a2
Joboto <joeisamonkey@gmail.com>
2015-04-15 09:51:16 +0100

    Correcting db schema
    Was missing CREATE DATABASE statement before

--- movement.sql
+++ movement.sql
@@ -1,24 +1,19 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 3.2.0.1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 14, 2015 at 01:24 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Host: localhost
+-- Generation Time: Apr 15, 2015 at 08:49 AM
+-- Server version: 5.1.36
+-- PHP Version: 5.3.0
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
 -- Database: `movement`
 --
+CREATE DATABASE `movement` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `movement`;
 
 -- --------------------------------------------------------
 
@@ -36,6 +31,9 @@ CREATE TABLE IF NOT EXISTS `members` (
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`fname`, `lname`, `email`, `bio`, `pword`, `username`) VALUES
+('Joe', 'Lear', 'joeisamonkey@yahoo.com', 'All about me', 'joe', '');
