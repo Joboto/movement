1adc5943100e3e26cc5f1a670f338976dfe451c4
Joboto <joeisamonkey@gmail.com>
2015-04-15 18:20:25 +0100

    Text for single item in basket
    Added switch statement for 'There is 1 item...'

--- includes/basket.php
+++ includes/basket.php
@@ -5,7 +5,16 @@ session_start();
 if(!isset($_SESSION['basketItems'])) {
 	echo 'Your basket is empty';
 } else {
-	echo 'There are ' . count($_SESSION['basketItems']) . ' items in your basket.';
+	switch(count($_SESSION['basketItems'])){
+	case 0:
+		echo 'Your basket is empty';
+		break;
+	case 1:
+		echo 'There is 1 item in your basket.';
+		break;
+	default:
+		echo 'There are ' . count($_SESSION['basketItems']) . ' items in your basket.';
+	}
 }
 ?>
 	</a>
