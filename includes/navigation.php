43b6d63016c8ef0b353c619a87d1f9ec55a3ab1a
Joboto <joeisamonkey@gmail.com>
2015-04-17 10:57:37 +0100

    Done. Submitted.
    Added final report to root folder.
    Updated links to original func reqs.
    Added link for Report doc.
    Deleted old php files (again).
    Forgot to remove confirm password field.

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,7 +1,8 @@
 <?php 
 echo '<div id="navigation">
 			<ul>
-				<li><a href="/movement/about.php">About</a></li>
+				<li><a href="/movement/about.php">About (Original functional requirements)</a></li>
+				<li><a href="/movement/FinalReport.docx">Final report (docx)</a></li>
 				<li><a href="/movement/courses">Courses</a></li>
 				<li><a href="/movement/equipment">Equipment</a></li>
 				<li><a href="/movement/events">Events</a></li>
